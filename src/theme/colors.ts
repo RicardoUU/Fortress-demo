0f8e235bb0f07daa62bdf5d43a82ab015dc8931c
Ricardo <Ricardo.chen@castlery.com>
2023-03-27 19:02:07 +0800

    feat: add theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,7 @@
+import { Colors } from './types/colors';
+
+const colors: Colors = {
+  PRIMARY: '#0056a9'
+}
+
+export default colors;
---
+++ src/theme/types/colors.ts
@@ -0,0 +1,4 @@
+export type Color =
+  | 'PRIMARY';
+
+export type Colors = { [key in Color]: string };
