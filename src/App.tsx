4e0663c188c11e593a8a5f594c3fc6c9656ca8e9
Ricardo <RicardoCzj@foxmail.com>
2023-10-19 14:29:43 +0800

    fix: change for test

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
-                change for test 1.x xxxx
+                change for test 1.x
             </p>
 
         </div>
