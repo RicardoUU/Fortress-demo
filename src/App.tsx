a311f15d3600ee8476ae1342c4a479d2c5b186f4
Ricardo <RicardoCzj@foxmail.com>
2023-10-25 15:20:13 +0800

    feat: commit for test
    fix: commit for test
    
    chore: commit for test chore

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
                 change for test 1.x xxx
+                xx 
             </p>
 
         </div>
